5769219e7e467139dd5f0be142650ce50c9bc43e
Nemo <ahmedghanem44@yahoo.com>
2019-09-09 00:32:15 -0400

    PreFinalizing

--- Client/NGOServices/src/app/app.module.ts
+++ Client/NGOServices/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UserListComponent } from './user-list/user-list.component';
+
 import { EventService } from './event.service';
 import { SignupFromComponent } from './signup-from/signup-from.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
@@ -17,18 +17,16 @@ import { EventsForDonationComponent } from './events-for-donation/events-for-don
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { SigninComponent } from './signin/signin.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
+
 import { AddEventComponent } from './add-event/add-event.component';
 import { AdminUserEditComponent } from './admin-user-edit/admin-user-edit.component';
 import { CartComponent } from './cart/cart.component';
 import { DonationMngComponent } from './donation-mng/donation-mng.component';
 import { EventMngComponent } from './event-mng/event-mng.component';
 import { HomeComponent } from './home/home.component';
-import { ListOfEventsComponent } from './list-of-events/list-of-events.component';
+
 import { MakeDonationComponent } from './make-donation/make-donation.component';
-import { UserHomeComponent } from './user-home/user-home.component';
-import { UserInfoComponent } from './user-info/user-info.component';
+
 import { UserMngComponent } from './user-mng/user-mng.component';
 import { HomeInComponent } from './home-in/home-in.component';
 import { EditEventComponent } from './edit-event/edit-event.component';
@@ -47,25 +45,23 @@ import { CartService } from './cart.service';
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
+
     SignupFromComponent,
     EventsForDonationComponent,
     UserProfileComponent,
     SigninComponent,
     NavBarComponent,
-    AdminComponent,
+
     HomeComponent,
     UserMngComponent,
     DonationMngComponent,
     MakeDonationComponent,
     CartComponent,
-    UserInfoComponent,
+
     AddEventComponent,
     EventMngComponent,
     AdminUserEditComponent,
-    UserHomeComponent,
-    ListOfEventsComponent,
-    LoginComponent,
+ 
     HomeInComponent,
     EditEventComponent,
     ErrorComponent,
@@ -87,10 +83,7 @@ import { CartService } from './cart.service';
     MatIconModule,
     // FormBuilder
     RouterModule.forRoot([
-      {
-        path: 'login',
-        component:LoginComponent
-      },
+
       {
         path: 'signin', // nemo
         component:HomeComponent
@@ -133,10 +126,7 @@ import { CartService } from './cart.service';
         component: UserMngComponent,
         canActivate : [AdminGuardService]
       },
-      {
-        path: 'admin',
-        component: AdminComponent
-      },
+
       {
         path: 'donation_mng',
         component: DonationMngComponent,
@@ -150,10 +140,7 @@ import { CartService } from './cart.service';
         path: 'cart',
         component: CartComponent
       },
-      {
-        path: 'user_info',
-        component: UserInfoComponent
-      },
+
       {
         path: 'add_event',
         component: AddEventComponent,
